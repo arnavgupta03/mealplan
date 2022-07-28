8cff92b13e7c6d7cb128e1f95c07de4a75c0c168
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2022-07-27 23:27:52 -0400

    Add MainLayout with link to HomePage

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -1,9 +1,32 @@
+import { Link, routes } from '@redwoodjs/router'
+
 type MainLayoutProps = {
   children?: React.ReactNode
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return <>{children}</>
+  return (
+    <>
+      <header>
+        <nav
+          className="navbar navbar-expand-lg navbar-dark"
+          style={{ 'background-color': '#6b007d' }}
+        >
+          <h1 className="navbar-brand mx-3 my-2">
+            <strong>MealPlan</strong>
+          </h1>
+          <ul className="navbar-nav">
+            <li className="nav-item mx-2">
+              <Link className="nav-link" to={routes.home()}>
+                <strong>Week Plan</strong>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
 }
 
 export default MainLayout
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -6,14 +6,7 @@ const HomePage = () => {
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1>MealPlan</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <p>This page works.</p>
     </>
   )
 }
