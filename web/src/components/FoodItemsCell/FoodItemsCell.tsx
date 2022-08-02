a13471adf9ad48412695a3b6f0555b45283375e5
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2022-08-02 15:51:42 -0400

    Set successful food items return to single div

--- web/src/components/FoodItemsCell/FoodItemsCell.tsx
+++ web/src/components/FoodItemsCell/FoodItemsCell.tsx
@@ -1,5 +1,6 @@
 import type { FoodItemsQuery } from 'types/graphql'
 
+import { Link, routes } from '@redwoodjs/router'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
 export const QUERY = gql`
@@ -13,18 +14,31 @@ export const QUERY = gql`
 
 export const Loading = () => <div>Loading...</div>
 
-export const Empty = () => <div></div>
+export const Empty = () => (
+  <div>
+    <li className="list-group-item">No Food Items Yet, Create One!</li>
+  </div>
+)
 
 export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
 export const Success = ({ foodItems }: CellSuccessProps<FoodItemsQuery>) => {
-  return foodItems.map((item) => {
-    return (
-      <li key={item.id} className="list-group-item list-group-item-action">
-        {item.name}
-      </li>
-    )
-  })
+  return (
+    <div>
+      {foodItems.map((item) => {
+        return (
+          <li key={item.id} className="list-group-item d-grid gap-2">
+            <Link
+              to={routes.foodItem({ id: item.id })}
+              className="btn btn-outline-dark"
+            >
+              {item.name}
+            </Link>
+          </li>
+        )
+      })}
+    </div>
+  )
 }
