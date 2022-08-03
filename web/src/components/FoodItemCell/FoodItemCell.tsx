0160712523af1a26de51c513b7816e2ebe0fd889
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2022-08-03 16:44:21 -0400

    Move location of add ingredient button

--- web/src/components/FoodItemCell/FoodItemCell.tsx
+++ web/src/components/FoodItemCell/FoodItemCell.tsx
@@ -1,10 +1,18 @@
+import ReactModal from 'react-modal'
 import type {
   FindFoodItemQuery,
   FindFoodItemQueryVariables,
 } from 'types/graphql'
 
-import { MetaTags, useMutation } from '@redwoodjs/web'
+import {
+  Form,
+  NumberField,
+  Submit,
+  TextField,
+  FormError,
+} from '@redwoodjs/forms'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import { MetaTags, useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
 
 export const QUERY = gql`
@@ -17,6 +25,16 @@ export const QUERY = gql`
         name
         quantity
       }
+    }
+  }
+`
+
+const CREATE_INGREDIENT = gql`
+  mutation CreateIngredientMutation($input: CreateIngredientInput!) {
+    createIngredient(input: $input) {
+      id
+      name
+      quantity
     }
   }
 `
@@ -47,6 +65,40 @@ export const Failure = ({
 export const Success = ({
   foodItem,
 }: CellSuccessProps<FindFoodItemQuery, FindFoodItemQueryVariables>) => {
+  const [modalIsOpen, setOpen] = React.useState(false)
+
+  const [create, { loading, error }] = useMutation(CREATE_INGREDIENT, {
+    onCompleted: async () => {
+      toast.success('New Ingredient Created', {
+        duration: 2000,
+      })
+    },
+    refetchQueries: [
+      { query: QUERY }, // DocumentNode object parsed with gql
+      'FindFoodItemQuery', // Query name
+    ],
+  })
+
+  const handleOpenModal = () => {
+    setOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setOpen(false)
+  }
+
+  const onSubmit = (data) => {
+    data.foodItemId = foodItem.id
+    console.log(data)
+    const ingredientList = []
+    for (const ing of ingredientList) {
+      ingredientList.push(ing)
+    }
+    create({ variables: { input: data } })
+      .then((newIngredient) => ingredientList.push(newIngredient.data))
+      .then(() => setOpen(false))
+  }
+
   const [delIng] = useMutation(DELETE_INGREDIENT, {
     onCompleted: async () => {
       toast.success('Ingredient deleted for ' + foodItem.name, {
@@ -67,11 +119,58 @@ export const Success = ({
     <div className="container-fluid">
       <MetaTags title={foodItem.name} description="FoodItem page" />
       <div className="row">
-        <div className="col-md-12">
+        <div className="col-md-9">
           <h1 className="my-3 mx-2">{foodItem.name}</h1>
+        </div>
+        <div className="col-md-3 d-grid gap-2">
+          <button
+            type="button"
+            className="btn btn-success btn-lg my-4 mx-2"
+            onClick={handleOpenModal}
+          >
+            + Add Ingredient
+          </button>
         </div>
       </div>
       <div className="row mx-1 my-2">
+        <ReactModal isOpen={modalIsOpen} ariaHideApp={false}>
+          <Form
+            className="my-3 container-fluid"
+            onSubmit={onSubmit}
+            error={error}
+          >
+            <FormError error={error} wrapperClassName="form-error" />
+            <label htmlFor="name" className="form-label">
+              Ingredient Name
+            </label>
+            <TextField
+              className="row mb-3 form-control"
+              name="name"
+              validation={{ required: true }}
+            />
+
+            <label htmlFor="quantity" className="form-label">
+              Ingredient Quantity
+            </label>
+            <NumberField
+              className="row mb-3 form-control"
+              name="quantity"
+              validation={{ required: true }}
+            />
+            <div className="row">
+              <Submit className="btn btn-success my-2" disabled={loading}>
+                Add Ingredient
+              </Submit>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={handleCloseModal}
+              >
+                Close
+              </button>
+            </div>
+          </Form>
+        </ReactModal>
         <ul className="list-group list-group-flush">
           {foodItem.ingredients.map((ingredient) => {
             return (
