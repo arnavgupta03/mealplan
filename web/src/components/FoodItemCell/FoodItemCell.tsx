05ae8ca3d286a130937003cedd35885a1a6c6828
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2022-08-03 16:16:54 -0400

    Add functionality to create and delete ingredients

--- web/src/components/FoodItemCell/FoodItemCell.tsx
+++ web/src/components/FoodItemCell/FoodItemCell.tsx
@@ -3,8 +3,9 @@ import type {
   FindFoodItemQueryVariables,
 } from 'types/graphql'
 
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useMutation } from '@redwoodjs/web'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/toast'
 
 export const QUERY = gql`
   query FindFoodItemQuery($id: String!) {
@@ -20,6 +21,19 @@ export const QUERY = gql`
   }
 `
 
+const DELETE_INGREDIENT = gql`
+  mutation DeleteIngredientMutation($id: String!) {
+    deleteIngredient(id: $id) {
+      id
+      name
+    }
+  }
+`
+
+export const beforeQuery = (props) => {
+  return { variables: props, fetchPolicy: 'cache-and-network' }
+}
+
 export const Loading = () => <div>Loading...</div>
 
 export const Empty = () => <div>No Food Item at this page</div>
@@ -33,25 +47,46 @@ export const Failure = ({
 export const Success = ({
   foodItem,
 }: CellSuccessProps<FindFoodItemQuery, FindFoodItemQueryVariables>) => {
+  const [delIng] = useMutation(DELETE_INGREDIENT, {
+    onCompleted: async () => {
+      toast.success('Ingredient deleted for ' + foodItem.name, {
+        duration: 2000,
+      })
+    },
+    refetchQueries: [
+      { query: QUERY }, // DocumentNode object parsed with gql
+      'FindFoodItemQuery', // Query name
+    ],
+  })
+
+  const handleDelete = (ingId) => {
+    delIng({ variables: { id: ingId } })
+  }
+
   return (
     <div className="container-fluid">
       <MetaTags title={foodItem.name} description="FoodItem page" />
       <div className="row">
-        <div className="col-md-9">
+        <div className="col-md-12">
           <h1 className="my-3 mx-2">{foodItem.name}</h1>
-        </div>
-        <div className="col-md-3 d-grid gap-2">
-          <button type="button" className="btn btn-success btn-lg my-4 mx-2">
-            + Add Ingredient
-          </button>
         </div>
       </div>
       <div className="row mx-1 my-2">
         <ul className="list-group list-group-flush">
           {foodItem.ingredients.map((ingredient) => {
             return (
-              <li key={ingredient.id} className="list-group-item lead">
+              <li
+                key={ingredient.id}
+                className="list-group-item lead d-flex justify-content-between"
+              >
                 {ingredient.quantity + ' x ' + ingredient.name}
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(ingredient.id)}
+                >
+                  Delete
+                </button>
               </li>
             )
           })}
--- web/src/pages/FoodItemPage/FoodItemPage.tsx
+++ web/src/pages/FoodItemPage/FoodItemPage.tsx
@@ -1,13 +1,116 @@
-import FoodItemCell from 'src/components/FoodItemCell'
+import ReactModal from 'react-modal'
 
+import {
+  Form,
+  NumberField,
+  Submit,
+  TextField,
+  FormError,
+} from '@redwoodjs/forms'
+import { useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/toast'
+
+import FoodItemCell, { QUERY } from 'src/components/FoodItemCell'
 interface Props {
   id: string
 }
 
+const CREATE_INGREDIENT = gql`
+  mutation CreateIngredientMutation($input: CreateIngredientInput!) {
+    createIngredient(input: $input) {
+      id
+      name
+      quantity
+    }
+  }
+`
+
 const FoodItemPage = ({ id }: Props) => {
+  const [modalIsOpen, setOpen] = React.useState(false)
+
+  const [create, { loading, error }] = useMutation(CREATE_INGREDIENT, {
+    onCompleted: async () => {
+      toast.success('New Ingredient Created', {
+        duration: 2000,
+      })
+    },
+    refetchQueries: [
+      { query: QUERY }, // DocumentNode object parsed with gql
+      'FindFoodItemQuery', // Query name
+    ],
+  })
+
+  const handleOpenModal = () => {
+    setOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setOpen(false)
+  }
+
+  const onSubmit = (data) => {
+    data.foodItemId = id
+    console.log(data)
+    const ingredientList = []
+    for (const ing of ingredientList) {
+      ingredientList.push(ing)
+    }
+    create({ variables: { input: data } })
+      .then((newIngredient) => ingredientList.push(newIngredient.data))
+      .then(() => setOpen(false))
+  }
+
   return (
     <>
+      <Toaster />
       <FoodItemCell id={id} />
+      <div className="col-md-12 d-grid gap-2">
+        <button
+          type="button"
+          className="btn btn-success btn-lg my-4 mx-2"
+          onClick={handleOpenModal}
+        >
+          + Add Ingredient
+        </button>
+      </div>
+      <ReactModal isOpen={modalIsOpen} ariaHideApp={false}>
+        <Form
+          className="my-3 container-fluid"
+          onSubmit={onSubmit}
+          error={error}
+        >
+          <FormError error={error} wrapperClassName="form-error" />
+          <label htmlFor="name" className="form-label">
+            Ingredient Name
+          </label>
+          <TextField
+            className="row mb-3 form-control"
+            name="name"
+            validation={{ required: true }}
+          />
+
+          <label htmlFor="quantity" className="form-label">
+            Ingredient Quantity
+          </label>
+          <NumberField
+            className="row mb-3 form-control"
+            name="quantity"
+            validation={{ required: true }}
+          />
+          <div className="row">
+            <Submit className="btn btn-success my-2" disabled={loading}>
+              Add Ingredient
+            </Submit>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={handleCloseModal}
+            >
+              Close
+            </button>
+          </div>
+        </Form>
+      </ReactModal>
     </>
   )
 }
